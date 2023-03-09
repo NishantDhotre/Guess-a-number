ee46ab8d58e2bd196f89f39ddbb84f1ee0a04794
Nishant <dhotrenishant@gmail.com>
2023-03-09 10:37:33 +0530

    renaming and buttoms alignment

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -5,68 +5,65 @@ import tkinter
 
 def start():
     h = random.randrange(1, 201)
-    bt.destroy()
-    lbll3.destroy()
+    startButton.destroy()
+    heading.destroy()
 
     def result(x):
         hidden = h
         if x == hidden:
-            lbl2.configure(text="Nailed it!")
-            btnn.destroy()
+            heading2.configure(text="Nailed it!")
+            endButton.destroy()
         elif x < hidden:
-            lbl2.configure(text="Your guess is too low")
+            heading2.configure(text="Your guess is too low")
         else:
-            lbl2.configure(text="Your guess is too high")
+            heading2.configure(text="Your guess is too high")
 
-    lbl = Label(window, text="enter you'r guess:  ", fg="green")
-    lbl.grid(column=1, row=4)
-    lbl2 = Label(window, text="\" Let's start the game\"", fg="black")
-    lbl2.grid(column=2, row=1)
+    instructionPromt = Label(window, text="enter you'r guess:  ", fg="green")
+    instructionPromt.place(relx=0.2, rely=0.4, anchor=CENTER)
+    heading2 = Label(window, text="\" Let's start the game\"", fg="black")
+    heading2.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-    lbl3 = Label(window, text="")
-    lbl3.grid(column=1, row=2)
-    lbl4 = Label(window, text="")
-    lbl4.grid(column=1, row=3)
-    lbl6 = Label(window, text="")
-    lbl6.grid(column=1, row=5)
 
-    txt = Entry(window, width=10, bg="white")
-    txt.focus()
-    txt.grid(column=2, row=4)
+
+    inputSection = Entry(window, width=10, bg="white")
+    inputSection.focus()
+    inputSection.place(relx=0.5, rely=0.4, anchor=CENTER)
 
     def up():
-        lbl.destroy()
-        btn.destroy()
-        txt.destroy()
-        btnn.destroy()
-        lbl2.configure(text="   the ans = " + str(h))
+        instructionPromt.destroy()
+        submitButton.destroy()
+        inputSection.destroy()
+        endButton.destroy()
+        heading2.configure(text="   the ans = " + str(h))
         return
 
     def clicked():
-        x = int(txt.get())
+        x = int(inputSection.get())
         result(x)
 
-    btn = Button(window,
+    submitButton = Button(window,
                  text="submit",
                  bg="green",
                  fg="white",
                  command=clicked)
-    btn.grid(column=3, row=4)
-    btnn = Button(window, text="Give up.!", fg="red", command = up)
-    btnn.grid(column=2, row=6)
+    submitButton.place(relx=0.8, rely=0.4, anchor=CENTER)
+    endButton = Button(window, text="Give up.!", fg="red", command = up)
+    endButton.place(relx=0.5, rely=0.6, anchor=CENTER)
 
 
 if __name__ == "__main__":
     window = Tk()
     window.title("Guess It..!")
     window.geometry('350x200')
-    lbll3 = Label(window, text="\"Guess the Number\"")
-    lbll3.pack()
-    bt = tkinter.Button(window,
+    heading = Label(window, text="\"Guess the Number\"")
+    heading.pack()
+    heading.place(relx=0.5, rely=0.3, anchor=CENTER)
+    startButton = tkinter.Button(window,
                         text=">start<",
                         bg="gray",
                         fg="white",
                         command=start)
-    bt.pack()
-    bt.flash()
+    startButton.pack()
+    startButton.flash()
+    startButton.place(relx=0.5, rely=0.5, anchor=CENTER)
     window.mainloop()
